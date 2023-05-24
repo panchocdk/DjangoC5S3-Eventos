d3aa52162454714fe2d3eed94cfc4260bc2c34b4
Francisco Lopez <panchocdk@hotmail.com>
2023-05-24 16:06:54 -0300

    Formulario y templatepara crear Clientes

--- reservas/forms.py
+++ reservas/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Empleado
+from .models import Empleado, Cliente
 
 class EmpleadoForm(forms.ModelForm):
 
     class Meta:
         model = Empleado
         fields ='nombre','apellido','numero_legajo'
+
+
+class ClienteForm(forms.ModelForm):
+
+    class Meta:
+        model = Cliente
+        fields ='nombre','apellido'
