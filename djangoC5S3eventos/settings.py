f1903c4d6849bbc69dbacfc105b6a198eba3ec4b
Lucas Roman <107078308+lucasromanh@users.noreply.github.com>
2023-06-07 04:11:18 -0300

    AHORA SI SE VIENE LO CHIDOO

--- djangoC5S3eventos/settings.py
+++ djangoC5S3eventos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangoC5S3eventos',
     'reservas',
     'api',
 ]
@@ -57,9 +58,18 @@ ROOT_URLCONF = 'djangoC5S3eventos.urls'
 
 TEMPLATES = [
     {
-        'BACKEND':'django.template.backends.jinja2.Jinja2',
-        'DIRS':[os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS':True,
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'environment': 'djangoC5S3eventos.jinja2.ini_jinja2',
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -124,7 +134,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
